98707169973c3cd9a0c86b0db0a1c19582e5e49a
martindwyer <martin.dwyer@bdo-tech.com>
2020-11-30 17:46:46 -0600

    Updates

--- src/config/prod.js
+++ src/config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
   port: process.env.PORT,
   jwtSecretKey: process.env.JWT_SECRET_KEY,
   sendGridAPI: process.env.SENDGRID_API,
-  mongoURL: process.end.MONGO_URL,
+  mongoURL: process.env.MONGO_URL,
 };
